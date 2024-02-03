65d56076bf151f957ee26f6c88d8ce3931f9f915
Eric Maranga <ekirira22@gmail.com>
2024-02-03 04:28:02 +0300

    completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,11 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
     return (
-        <div></div>
+        <form>
+            <input type="password" onChange={() => {console.log("Entering password...")}} placeholder="Enter password"/>
+        </form>
     )
 }
 
